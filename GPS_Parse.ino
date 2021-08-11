f2267d0f0821ebccb6b004a61de298ded9b347a9
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2021-08-11 16:48:30 -0700

    Update GPS_Parse.ino

--- GPS_Parse.ino
+++ GPS_Parse.ino
@@ -1,7 +1,7 @@
 //GGA Assignments
 String inByte;
 char Byte;
-char buff[12][12];
+char buff1[12][12];
 float UTC = 0;//Time
 float LAT = 0; //Latitude
 char LATDir = 'I'; //Direction
@@ -16,6 +16,7 @@ float Altref = 0; //Geoid Separation
 char uSep = 'I'; //Units of Separation
 float diffAge = 0; //Age of Differential Connections
 //VTG Assignments
+char buff2[8][12];
 float cogt = 0; //Course over ground (true)
 char T = 'I'; //Fixed Field: Truth
 float cogm = 0; //Course over ground (magnetic)
@@ -39,46 +40,42 @@ void loop() {
     Byte = Serial1.read();
     if (Byte == '$')//start of new data
     {
-      inByte = Serial1.readStringUntil('\n');//read until return
-      parseData();//parse data into buff
+      inByte = Serial1.readStringUntil(13);//read until return
+      if (inByte.startsWith("$GPGGA")
+          parseData(buff1);//parse data into buff1 
+      else if (inByte.startsWith("$GPVTG"){
+        parseData(buff2);
     }
   }
-  //Serial.println(buff[0]);
-  if (inByte[3] == 'G')//Handle GPGGA
-  {
-    UTC = atof(buff[0]);
-    LAT = atof(buff[1]);
-    LATDir = buff[2][0];
-    LON = atof(buff[3]);
-    LONDir = buff[4][0];
-    fixStatus = atoi(buff[5]);
-    NoSVs = atoi(buff[6]);
-    HDop = atof(buff[7]);
-    ALT = atof(buff[8]);
-    ALTUnit = buff[9][0];
-    Altref = atof(buff[10]);
-    uSep = buff[11][0];
-    diffAge = atof(buff[12]);
-  }
-  else if (inByte[3] == 'T')//Handle GPVTG
-  {
-    cogt = atof(buff[0]); //Course over ground (true)
-    T = buff[1][0]; //Fixed Field: Truth
-    cogm = atof(buff[2]);; //Course over ground (magnetic)
-    M = buff[3][0]; // Fixed Field: Magnetic
-    sog = atof(buff[4]);; //Speed over ground (knots)
-    N = buff[5][0]; // Fided Field: knots
-    kph = atof(buff[6]);; //Speed over ground (kph)
-    K = buff[7][0]; //Fixed Field: Kilometers per Hour
-    mode = buff[8][0]; //Mode Indicator
-  }
+  UTC = atof(buff1[0]);
+  LAT = atof(buff1[1]);
+  LATDir = buff1[2][0];
+  LON = atof(buff1[3]);
+  LONDir = buff1[4][0];
+  fixStatus = atoi(buff1[5]);
+  NoSVs = atoi(buff1[6]);
+  HDop = atof(buff1[7]);
+  ALT = atof(buff1[8]);
+  ALTUnit = buff1[9][0];
+  Altref = atof(buff1[10]);
+  uSep = buff1[11][0];
+  diffAge = atof(buff1[12]); 
+  cogt =  atof(buff2[0]);; //Course over ground (true)
+  T = buff2[1][0];; //Fixed Field: Truth
+  cogm = atof(buff2[2]); //Course over ground (magnetic)
+  M = buff2[3][0]; // Fixed Field: Magnetic
+  sog = atof(buff2[4]; //Speed over ground (knots)
+  N = buff2[5][0]; // Fided Field: knots
+  kph = atof(buff2[6]); //Speed over ground (kph)
+  K = buff2[7][0]; //Fixed Field: Kilometers per Hour
+  mode = buff2[8][0]; //Mode Indicator
   Serial.println(LAT);
   Serial.println(LON);
   Serial.println(ALT);
   Serial.println(kph);
   Serial.println(cogt);
 }
-void parseData()
+void parseData(char buff[][12])
 {
   int j = 0;
   int k = 0;;
